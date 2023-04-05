7ed4014f7860f80b5d3ed7ac2630a1513bb6dc76
George Truong <truong.george@gmail.com>
2023-04-04 18:33:07 -0700

    Fix some bugs and polish before final build

--- ToonTanks/Source/ToonTanks/Projectile.cpp
+++ ToonTanks/Source/ToonTanks/Projectile.cpp
@@ -46,14 +46,14 @@ void AProjectile::OnHit(
 	UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
 	FVector NormalImpulse, const FHitResult& Hit
 ) {
-	auto MyOwner = GetOwner();
+	AActor* MyOwner = GetOwner();
 	if (MyOwner == nullptr) {
 		Destroy();
 		return;
 	}
 
-	auto MyOwnerInstigator = MyOwner->GetInstigatorController();
-	auto DamageTypeClass = UDamageType::StaticClass();
+	AController* MyOwnerInstigator = MyOwner->GetInstigatorController();
+	UClass* DamageTypeClass = UDamageType::StaticClass();
 
 	if (OtherActor && OtherActor != this && OtherActor != MyOwner) {
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
