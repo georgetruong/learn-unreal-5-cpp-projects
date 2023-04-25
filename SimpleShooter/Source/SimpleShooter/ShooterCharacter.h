45ece184857c947d6130d79c5c4baabfcbb4bd49
George Truong <truong.george@gmail.com>
2023-04-24 17:27:10 -0700

    Create gun class and blueprint; Spawn on character at runtime

--- SimpleShooter/Source/SimpleShooter/ShooterCharacter.h
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+class AGun;
+
 UCLASS()
 class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -35,4 +37,10 @@ private:
 	UPROPERTY(EditAnywhere)
 	float RotationRateUp = 75.0f;
 	float RotationRateRight = 10.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AGun> GunClass;
+
+	UPROPERTY()
+	AGun* Gun;
 };
