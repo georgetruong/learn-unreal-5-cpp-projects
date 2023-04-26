43d8c86ab0440c388c664949e0263becbcf8f204
George Truong <truong.george@gmail.com>
2023-04-26 11:37:56 -0700

    Implement behavior tree and blackboard with basic patrol AI behavior

--- SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -3,21 +3,30 @@
 
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 void AShooterAIController::BeginPlay()
 {
     Super::BeginPlay();
 
-    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-    SetFocus(PlayerPawn);
+    if (AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
+
+        APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+    }
 }
 
 void AShooterAIController::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    
+    /**
 
     if (LineOfSightTo(PlayerPawn)) 
     {
@@ -29,4 +38,5 @@ void AShooterAIController::Tick(float DeltaTime)
         ClearFocus(EAIFocusPriority::Gameplay);
         StopMovement();
     }
+    **/
 }
--- SimpleShooter/Source/SimpleShooter/ShooterAIController.h
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.h
@@ -22,5 +22,5 @@ protected:
 
 private:
 	UPROPERTY(EditAnywhere)
-	float AcceptanceRadius = 300;
+	class UBehaviorTree* AIBehavior;
 };
